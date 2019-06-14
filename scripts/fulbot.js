1cdd0612f595dc5996ea2810b66c0834f4ae04fb
Nicolas Escalante <nlante@gmail.com>
2019-06-14 15:42:38 -0300

    starting

--- scripts/fulbot.js
+++ scripts/fulbot.js
@@ -1,3 +1,13 @@
 'use strict';
 
-module.exports = require('../src/commands');
+console.log('fulbot starting');
+
+const serviceFactory = require('../src/service');
+
+module.exports = function controller(robot) {
+  const service = serviceFactory(robot);
+
+  robot.hear(/(^lista$|^quienes (juegan|van){1}$)/i, service.getUsers);
+  robot.hear(/^(juego|voy|\+1)$/i, service.addUser);
+  robot.hear(/^(me bajo|-1|no juego|no voy)$/i, service.removeUser);
+};
