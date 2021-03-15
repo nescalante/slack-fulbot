a64861bc520f4553fa1cf01f6c8f66c4500246f8
Nicolas Escalante <nlante@gmail.com>
2021-03-15 13:50:50 -0300

    test

--- src/db.js
+++ src/db.js
@@ -1,8 +1,8 @@
-console.log('connecting using', `${process.env.DATABASE_URL}?sslmode=require`);
+console.log('connecting using', `${process.env.DATABASE_URL}?ssl=true`);
 
 const db = require('knex')({
   client: 'pg',
-  connection: `${process.env.DATABASE_URL}?sslmode=require`
+  connection: `${process.env.DATABASE_URL}?ssl=true`
 });
 
 module.exports = db;
